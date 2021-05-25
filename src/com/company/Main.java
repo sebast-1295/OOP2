0966f3d65b2d77ded8560f81ce29c5143bb40afa
sebast-1295 <sebashfm95@gmail.com>
2021-05-24 21:18:28 -0600

    Trabajo en clase 2 Commit 24-05-2020

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 import com.company.familia.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Sebas bello = new Sebas();
-        Martin papa = new Martin();
+        Martin papa = new Martin( "Hola");
+
+        papa.setAge();
+        System.out.println(papa.getAge());
+        System.out.println(papa.getHobby());
 
         System.out.println("Hola Profes, muchas gracias por la ayuda");
     }
--- src/com/company/familia/Martin.java
+++ src/com/company/familia/Martin.java
@@ -2,9 +2,26 @@ package com.company.familia;
 
 public class Martin {
 
-    public Martin(){
+    private int age;
+    private String hobby;
 
-        System.out.println("Mi papa");
+    public Martin(String saludo){
+        this.age = 55;
+        this.hobby = "leer";
+
+        System.out.println(saludo);
+    }
+
+    public void setAge(){
+        this.age = this.age + 1;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getHobby() {
+        return hobby;
     }
 
 }
